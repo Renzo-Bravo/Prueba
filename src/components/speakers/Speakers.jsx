793bf5ae0c4ae6d1b999a585503de7bd563dd6ec
Renzo-Bravo <renzobravogomez@gmail.com>
2025-12-17 20:37:41 -0300

    Update speakers section

--- src/components/speakers/Speakers.jsx
+++ src/components/speakers/Speakers.jsx
@@ -3,38 +3,38 @@ import "./Speakers.css";
 
 export default function Speakers() {
   const speakers = [
-    {
-      nombre: "Nombre y Apellido",
-      charla: "Título de charla",
-    },
-    {
-      nombre: "Nombre y Apellido",
-      charla: "Título de charla",
-    },
-    {
-      nombre: "Nombre y Apellido",
-      charla: "Título de charla",
-    },
+    { nombre: "Nombre y Apellido", charla: "Título de charla" },
+    { nombre: "Nombre y Apellido", charla: "Título de charla" },
+    { nombre: "Nombre y Apellido", charla: "Título de charla" },
   ];
 
   return (
     <section className="speakers-section" id="speakers">
-      <h2 className="title">Disertantes</h2>
+      <div className="calendar-header">
+        <h2 className="title">Disertantes</h2>
+        <div className="title-underline"></div>
+      </div>
 
       <div className="speakers-grid">
         {speakers.map((d, index) => (
-          <div key={index} className="speakers-card">
-            <div className="foto-placeholder">
-              <i className="fas fa-user-alt"></i>
+          <div key={index} className="speaker-card">
+            <div className="foto-wrapper">
+              <div className="foto-placeholder">
+                <i className="fas fa-user-alt"></i>
+              </div>
             </div>
-            <a
-              href="http://google.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h3>{d.nombre}</h3>
-            </a>
-            <p>{d.charla}</p>
+            
+            <div className="speaker-info">
+              <a
+                href="http://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="speaker-link"
+              >
+                <h3>{d.nombre}</h3>
+              </a>
+              <p>{d.charla}</p>
+            </div>
           </div>
         ))}
       </div>
